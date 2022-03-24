cddade118ee9e708b87be6d587ef7b986c162466
Ananta kumar singh <70097466+anantasingh-data-design@users.noreply.github.com>
2022-03-24 19:00:49 +0530

    update firebase

--- src/Firebase.js
+++ src/Firebase.js
@@ -3,13 +3,13 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBGt2mgmhbFcJjvLbAizIw9NjlJXSRsuqA",
+    apiKey: "AIzaSyBGt2LbAizIw9NjlJXSRsuqA",
     authDomain: "twitterappbuild.firebaseapp.com",
     projectId: "twitterappbuild",
     storageBucket: "twitterappbuild.appspot.com",
-    messagingSenderId: "326526487557",
-    appId: "1:326526487557:web:ab4a3544fb9ac193e6206c",
-    measurementId: "G-J1EJTWV4Z9",
+    messagingSenderId: "326557",
+    appId: "1:326557:web:ab4ac193e6206c",
+    measurementId: "G-J1EZ9",
 };
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
